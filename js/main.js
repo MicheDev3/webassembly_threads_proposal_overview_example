b401c71064436104d3a80c3bdea3da4d9f4cf4bf
MicheDev3 <25401873+MicheDev3@users.noreply.github.com>
2025-04-28 11:46:12 +0100

    experimenting (part 8)

--- js/main.js
+++ js/main.js
@@ -1,107 +1,68 @@
-import { jaiTojsString, wasmDebugBreak, writeToConsoleLog } from "/js/common.js";
-
-async function run(modules)
-{
-	const sharedModule = modules[0];
-	const atomicModule = modules[1];
-	const mainModule   = modules[2];
-	
-	const sharedInstance = new WebAssembly.Instance(sharedModule);
-	const memory = sharedInstance.exports.memory;
-	
-	// TODO: Have this in wasm directly
-	function memcmp(lhs, rhs, size)
-	{
-		// https://discord.com/channels/661732390355337246/1172463903943446548/1256763226847187127
-		// https://www.tutorialspoint.com/c_standard_library/c_function_memcmp.htm
-		const str1 = jaiTojsString(memory.buffer, lhs, size);
-		const str2 = jaiTojsString(memory.buffer, rhs, size);
-		if (str1 == str2)    { return  0; }
-		if (!(str1 && str2)) { return -1; }
-		if (str1 < str2)     { return -1; }
-		if (str1 > str2)     { return  1; }
-		
-		return -1;
-	};
-	
-	function wasmWriteString(count, data, toStandardError)
-	{
-		const value = jaiTojsString(memory.buffer, data, count);
-		writeToConsoleLog(value, toStandardError);
-	}
-	
-	const workers  = [];
-	const atomicInstance = new WebAssembly.Instance(atomicModule, {env: {memory: memory}});
-	const mainInstance = new WebAssembly.Instance(mainModule,
-		{
-			env:
-			{
-				memory: memory,
-				wasm_write_string: wasmWriteString,
-				wasm_debug_break:  wasmDebugBreak,
-				memcmp: memcmp,
-				sleep:  atomicInstance.exports.sleep,
-				wake:   atomicInstance.exports.wake,
-				wait:   atomicInstance.exports.wait,
-				lock:   atomicInstance.exports.lock,
-				unlock: atomicInstance.exports.unlock,
-				get_worker_count: () =>
-				{
-					const workerCount = window.navigator.hardwareConcurrency - 1;
-					
-					return BigInt(workerCount);
-				},
-				push_worker: (worker_index) =>
-				{
-					// console.log("pushing worker: %d\n", worker_index);
-					// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
-					const worker = new Worker("/js/worker.js", { type: "module" });
-					workers.push(
-						new Promise(
-							(resolve) =>
-							{
-								worker.postMessage(
-									{
-										modules: [atomicModule, mainModule],
-										imports: {env: {memory: memory}},
-										worker_index: worker_index,
-									}
-								);
-								worker.onmessage = (event) => resolve(event.data);
-							}
-						)
-					);
-				},
-			}
-		}
-	);
-	
-	mainInstance.exports.main(0, BigInt(0));
-	
-	// TODO(mike): Understand how to properly use Promise.all()
-	await Promise.all(workers)
-	.then(
-		(values) =>
-		{
-		}
-	);
-}
-
 async function main()
 {
 	if (window.crossOriginIsolated)
 	{
-		const sharedUri = "/public/shared.wasm";
 		const atomicUri = "/public/atomic.wasm";
 		const mainUri   = "/public/main.wasm"  ;
 		Promise.all(
 			[
-				WebAssembly.compileStreaming(fetch(sharedUri)),
 				WebAssembly.compileStreaming(fetch(atomicUri)),
 				WebAssembly.compileStreaming(fetch(mainUri  )),
 			]
 		)
-		.then ( (modules) => { run(modules);       } )
+		.then ( async (modules) => 
+		{
+			const atomicModule = modules[0];
+			const mainModule   = modules[1];
+			
+			const memory = new WebAssembly.Memory
+			(
+				{
+					"initial": BigInt(100),
+					"maximum": BigInt(100),
+					"shared": true,
+					"address": "i64"
+				}
+			);
+			
+			const workers  = [];
+			// const workerCount = window.navigator.hardwareConcurrency;
+			const workerCount = 2;
+			for (let i = 0; i < workerCount; i++)
+			{
+				// https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers
+				const worker = new Worker("/js/worker.js", { type: "module" });
+				workers.push(
+					new Promise(
+						(resolve) =>
+						{
+							worker.postMessage(
+								{
+									modules: [atomicModule, mainModule],
+									imports: {env: {memory: memory}},
+									worker_index: i,
+								}
+							);
+							worker.onmessage = (event) => resolve(event.data);
+							worker.onerror   = reject;
+						}
+					)
+				);
+			}
+			
+			// TODO: Understand why this is not triggered
+			await Promise.all(workers)
+			.then(function(data)
+				{
+					console.log("All workers terminated");
+				}
+			)
+			.catch(function(error)
+				{
+					// something went wrong
+				}
+			);
+		})
 		.catch( (err)     => { console.error(err); } )
 		;
 	}
@@ -112,3 +73,4 @@ async function main()
 }
 
 main();
+
--- js/worker.js
+++ js/worker.js
@@ -1,4 +1,68 @@
-import { jaiTojsString, wasmDebugBreak, writeToConsoleLog } from "/js/common.js";
+// console.log and console.error always add newlines so we need to buffer the output from write_string
+// to simulate a more basic I/O behavior. We’ll flush it after a certain time so that you still
+// see the last line if you forget to terminate it with a newline for some reason.
+let consoleBuffer = "";
+let consoleBufferIsStandardError;
+let consoleTimeout;
+const FLUSH_CONSOLE_AFTER_MS = 3;
+
+function writeToConsoleLog(str, to_standard_error)
+{
+	if (consoleBuffer && consoleBufferIsStandardError != to_standard_error)
+	{
+		flushBuffer();
+	}
+	
+	consoleBufferIsStandardError = to_standard_error;
+	const lines = str.split("\n");
+	for (let i = 0; i < lines.length - 1; i++)
+	{
+		consoleBuffer += lines[i];
+		flushBuffer();
+	}
+	
+	consoleBuffer += lines[lines.length - 1];
+	
+	clearTimeout(consoleTimeout);
+	if (consoleBuffer)
+	{
+		consoleTimeout = setTimeout(() => {
+			flushBuffer();
+		}, FLUSH_CONSOLE_AFTER_MS);
+	}
+	
+	function flushBuffer()
+	{
+		if (!consoleBuffer) return;
+		
+		if (consoleBufferIsStandardError)
+		{
+			console.error(consoleBuffer);
+		}
+		else
+		{
+			console.log(consoleBuffer);
+		}
+		
+		consoleBuffer = "";
+	}
+}
+
+// TODO: Properly understand this
+function jaiTojsString(buffer, pointer, length)
+{
+	const u8 = new Uint8Array(buffer)
+	const bytes = u8.subarray(Number(pointer), Number(pointer) + Number(length));
+	
+	// Create a temporary ArrayBuffer and copy the contents of the shared buffer
+	// into it.
+	const tempBuffer = new ArrayBuffer(Number(length));
+	const tempView = new Uint8Array(tempBuffer);
+	tempView.set(bytes);
+	
+	const decoder = new TextDecoder();
+	return decoder.decode(tempBuffer);
+}
 
 onmessage = async function(message)
 {
@@ -8,47 +72,40 @@ onmessage = async function(message)
 	const mainModule   = modules[1];
 	
 	const memory = imports["env"]["memory"];
-	
-	// TODO: Have this in wasm directly
-	function memcmp(lhs, rhs, size)
-	{
-		// https://discord.com/channels/661732390355337246/1172463903943446548/1256763226847187127
-		// https://www.tutorialspoint.com/c_standard_library/c_function_memcmp.htm
-		const str1 = jaiTojsString(memory.buffer, lhs, size);
-		const str2 = jaiTojsString(memory.buffer, rhs, size);
-		if (str1 == str2)    { return  0; }
-		if (!(str1 && str2)) { return -1; }
-		if (str1 < str2)     { return -1; }
-		if (str1 > str2)     { return  1; }
-		
-		return -1;
-	};
-	
-	function wasmWriteString(count, data, toStandardError)
-	{
-		const value = jaiTojsString(memory.buffer, data, count);
-		writeToConsoleLog(value, toStandardError);
-	}
-	
 	const atomicInstance = new WebAssembly.Instance(atomicModule, imports);
 	const mainInstance = new WebAssembly.Instance(mainModule,
 		{
 			env:
 			{
 				memory: memory,
-				wasm_write_string: wasmWriteString,
-				wasm_debug_break:  wasmDebugBreak,
-				memcmp: memcmp,
-				sleep:  atomicInstance.exports.sleep,
-				wake:   atomicInstance.exports.wake,
-				wait:   atomicInstance.exports.wait,
-				lock:   atomicInstance.exports.lock,
-				unlock: atomicInstance.exports.unlock,
-				get_worker_count: ()        => { debugger },
-				push_worker: (worker_index) => { debugger },
+				memcmp: (lhs, rhs, size) => 
+				{
+					// https://discord.com/channels/661732390355337246/1172463903943446548/1256763226847187127
+					// https://www.tutorialspoint.com/c_standard_library/c_function_memcmp.htm
+					const str1 = jaiTojsString(memory.buffer, lhs, size);
+					const str2 = jaiTojsString(memory.buffer, rhs, size);
+					if (str1 == str2)    { return  0; }
+					if (!(str1 && str2)) { return -1; }
+					if (str1 < str2)     { return -1; }
+					if (str1 > str2)     { return  1; }
+					
+					return -1;
+				},
+				wasm_write_string: (count, data, toStandardError) =>
+				{
+					const value = jaiTojsString(memory.buffer, data, count);
+					writeToConsoleLog(value, toStandardError);
+				},
+				wasm_debug_break: () => { debugger },
+				wasm_sleep:  atomicInstance.exports.sleep,
+				wasm_wake:   atomicInstance.exports.wake,
+				wasm_wait:   atomicInstance.exports.wait,
+				wasm_lock:   atomicInstance.exports.lock,
+				wasm_unlock: atomicInstance.exports.unlock,
 			}
 		}
 	);
 
-	mainInstance.exports.worker_main(worker_index);
+	mainInstance.exports.worker_main(BigInt(worker_index));
 }
+
